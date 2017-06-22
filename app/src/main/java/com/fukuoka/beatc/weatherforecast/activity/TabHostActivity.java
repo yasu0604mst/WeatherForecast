8ac27368b25ccd65bbcf2da1924a76e2cd7d72b7
yasu <yasu0604mst@gmail.com>
2017-06-23 08:53:24 +0900

    update

--- app/src/main/java/com/fukuoka/beatc/weatherforecast/activity/TabHostActivity.java
+++ app/src/main/java/com/fukuoka/beatc/weatherforecast/activity/TabHostActivity.java
@@ -3,27 +3,48 @@ package com.fukuoka.beatc.weatherforecast.activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.fukuoka.beatc.weatherforecast.R;
 import com.fukuoka.beatc.weatherforecast.fragment.PageFragment;
+import com.fukuoka.beatc.weatherforecast.models.NavigationManager;
 
 public class TabHostActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener,
-        PageFragment.OnFragmentInteractionListener {
+        PageFragment.OnFragmentInteractionListener,NavigationView.OnNavigationItemSelectedListener  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_host);
+
+        //アクションバーの設定
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //ドロアのトグル
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // ナビゲーションの設定
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         // xmlからTabLayoutの取得
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
@@ -73,5 +94,35 @@ public class TabHostActivity extends AppCompatActivity implements ViewPager.OnPa
     @Override
     public void onFragmentInteraction(Uri uri) {
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        NavigationManager.onNavigationItemSelected(this, item);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
